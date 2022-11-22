4b9b2ba8e3367c8c1b2ecb7da844ec7b3913b8cd
morganhah <morganhah99@gmail.com>
2022-11-22 13:35:53 -0500

    added kd ratio, character

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
   const [matchTime, setMatchTime] = useState("")
   const [roundWins, setRoundWins] = useState(0)
   const [roundLoses, setRoundLoses] = useState(0)
+  const [kills, setKills] = useState(0)
+  const [deaths, setDeaths] = useState(0)
+  const [character, setCharacter] = useState("")
 
   useEffect(() => {
   axios.get('https://api.henrikdev.xyz/valorant/v1/account/peanutdamage/ftp')
@@ -43,7 +46,15 @@ function App() {
     })
     axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
     .then(res => {
-      console.log(res.data.data[0].players.all_players[5].stats.kills)
+      setKills(res.data.data[0].players.all_players[3].stats.kills)
+    })
+    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
+    .then(res => {
+      setDeaths(res.data.data[0].players.all_players[3].stats.deaths)
+    })
+    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
+    .then(res => {
+      setCharacter(res.data.data[0].players.all_players[3].character)
     })
   }, [])
   return (
@@ -51,8 +62,10 @@ function App() {
       <div><p>Username: {userName}</p> Account level: {accountLevel}</div>
       <div className='currentRank'>Current Rank: {currentRank}</div>
       <div className='match'>Map: {mapName}</div>
+      <div className='character'>Character: {character}</div>
       <div className='matchTime'>Match Time: {matchTime}</div>
       <div className='matchScore'>Match Score: {roundWins} - {roundLoses}</div>
+      <div className='kd'>KD: {kills} - {deaths}</div>
     </div>
   );
 }
