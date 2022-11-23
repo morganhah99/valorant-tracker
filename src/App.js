4c6cf158f8d3adac98b691f8a21fd2aab8244aba
morganhah <morganhah99@gmail.com>
2022-11-23 15:18:36 -0500

    added search bar to search user's level, rank, and info about previous game

--- src/App.js
+++ src/App.js
@@ -1,71 +1,59 @@
 import axios from 'axios';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
 
   const [userName, setUserName] = useState("")
-  const [accountLevel, setAccountLevel] = useState(0)
-  const [currentRank, setCurrentRank] = useState("")
+  const [userTag, setUserTag] = useState("")
+  const [accountLevel, setAccountLevel] = useState("")
+  const [userRank, setUserRank] = useState("")
   const [mapName, setMapName] = useState("")
   const [matchTime, setMatchTime] = useState("")
-  const [roundWins, setRoundWins] = useState(0)
-  const [roundLoses, setRoundLoses] = useState(0)
-  const [kills, setKills] = useState(0)
-  const [deaths, setDeaths] = useState(0)
-  const [character, setCharacter] = useState("")
+  const [roundWins, setRoundWins] = useState("")
+  const [roundLoses, setRoundLoses] = useState("")
 
-  useEffect(() => {
-  axios.get('https://api.henrikdev.xyz/valorant/v1/account/peanutdamage/ftp')
-    .then(res => {
-      setUserName(res.data.data.name)
-    })
-  axios.get('https://api.henrikdev.xyz/valorant/v1/account/peanutdamage/ftp')
+  function getUserInfoRequest() {
+    axios.get(`https://api.henrikdev.xyz/valorant/v1/account/${userName}/${userTag}`)
     .then(res => {
       setAccountLevel(res.data.data.account_level)
     })
-    axios.get('https://api.henrikdev.xyz/valorant/v1/mmr-history/na/peanutdamage/ftp')
+    axios.get(`https://api.henrikdev.xyz/valorant/v2/mmr/na/${userName}/${userTag}`)
     .then(res => {
-      setCurrentRank(res.data.data[0].currenttierpatched)
+      setUserRank(res.data.data.current_data.currenttierpatched)
     })
-    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
+    axios.get(`https://api.henrikdev.xyz/valorant/v3/matches/na/${userName}/${userTag}?filter=competitive`)
     .then(res => {
       setMapName(res.data.data[0].metadata.map)
     })
-    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
+    axios.get(`https://api.henrikdev.xyz/valorant/v3/matches/na/${userName}/${userTag}?filter=competitive`)
     .then(res => {
       setMatchTime(res.data.data[0].metadata.game_start_patched)
     })
-    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
+    axios.get(`https://api.henrikdev.xyz/valorant/v3/matches/na/${userName}/${userTag}?filter=competitive`)
     .then(res => {
       setRoundWins(res.data.data[0].teams.red.rounds_won)
     })
-    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
+    axios.get(`https://api.henrikdev.xyz/valorant/v3/matches/na/${userName}/${userTag}?filter=competitive`)
     .then(res => {
       setRoundLoses(res.data.data[0].teams.red.rounds_lost)
     })
-    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
-    .then(res => {
-      setKills(res.data.data[0].players.all_players[3].stats.kills)
-    })
-    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
-    .then(res => {
-      setDeaths(res.data.data[0].players.all_players[3].stats.deaths)
-    })
-    axios.get('https://api.henrikdev.xyz/valorant/v3/matches/na/peanutdamage/ftp?filter=competitive')
-    .then(res => {
-      setCharacter(res.data.data[0].players.all_players[3].character)
-    })
-  }, [])
+    
+  }
+
   return (
     <div className="App">
-      <div><p>Username: {userName}</p> Account level: {accountLevel}</div>
-      <div className='currentRank'>Current Rank: {currentRank}</div>
-      <div className='match'>Map: {mapName}</div>
-      <div className='character'>Character: {character}</div>
-      <div className='matchTime'>Match Time: {matchTime}</div>
-      <div className='matchScore'>Match Score: {roundWins} - {roundLoses}</div>
-      <div className='kd'>KD: {kills} - {deaths}</div>
+      <input type="text" placeholder="username" onChange={e => setUserName(e.target.value)} />
+      <input type="text" placeholder="tag" onChange={e => setUserTag(e.target.value)} />
+      <button onClick={getUserInfoRequest}>submit</button>
+      <div>Account Level: {accountLevel}</div>
+      <div>Account Rank: {userRank}</div>
+      <div className='headers'>Last Match</div>
+      <div className='history'>
+          <p>{mapName}</p>
+          <p>{matchTime}</p>
+          <p>{roundWins}:{roundLoses}</p>
+      </div>
     </div>
   );
 }
